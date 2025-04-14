4495bb28e1f353f2470fe7709acfd50be8312df0
TienNguyen93 <nguyentien9933@gmail.com>
2025-04-14 18:35:47 -0400

    remove my code

--- pl_modules/lstm_module.py
+++ pl_modules/lstm_module.py
@@ -21,7 +21,6 @@ class LSTMModule(BaseModule):
         optimizer='adam',
         mode='default',
         loss='rmse',
-        use_checkpoint=False, # my code
         **kwargs
     ): 
         super().__init__(lr=lr,
@@ -41,6 +40,4 @@ class LSTMModule(BaseModule):
             hidden_size=hidden_size,
             num_layers=num_layers,
             bidirectional=bidirectional,
-            use_checkpoint=use_checkpoint, # my code
-            **kwargs  # my code
         )
